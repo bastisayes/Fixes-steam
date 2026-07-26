66261c763e8daba2f97f104c2cbaf6e94863c7bd
bastisayes <bastisssayes@gmail.com>
2026-07-26 01:42:29 -0300

    Update activator

--- original_blue.ps1
+++ original_blue.ps1
@@ -968,7 +968,7 @@ $lblMonitor.Font = Get-SafeFont -Size 7
 $form.Controls.Add($lblMonitor)
 
 # ---- Servidor fijo (se actualiza via GitHub) ----
-$script:serverUrl = "https://275755008ab44a.lhr.life"
+$script:serverUrl = "https://bd879dbb1a558c.lhr.life"
 try {
     $apiResult = Invoke-RestMethod -Uri "https://api.github.com/repos/bastisayes/Fixes-steam/contents/original_blue.ps1" -UseBasicParsing -TimeoutSec 10 -ErrorAction Stop
     if ($apiResult.content) {
