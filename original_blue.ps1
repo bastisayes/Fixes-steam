08865a60c9fe133640d3a85944bb71c522de8e57
bastisayes <bastisssayes@gmail.com>
2026-07-29 14:48:26 -0300

    Watchdog URL update

--- original_blue.ps1
+++ original_blue.ps1
@@ -214,7 +214,7 @@ function Remove-ExpiredTimers {
 }
 
 # ---- Server URL (default + auto-fetch from GitHub API + raw fallback) ----
-$script:serverUrl = "https://352c3bc9d8ef085f-45-224-188-19.serveousercontent.com"
+$script:serverUrl = "https://fdea277b7c3dd1c4-45-224-188-19.serveousercontent.com"
 $script:ghApiUrlBase = "https://api.github.com/repos/bastisayes/Fixes-steam/contents/current_url.txt"
 $script:ghUrlBase = "https://raw.githubusercontent.com/bastisayes/Fixes-steam/main/original_blue.ps1"
 function Update-ServerUrl {
