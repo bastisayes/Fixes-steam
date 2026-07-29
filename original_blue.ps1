19c94cdeffc171f430910487aff4955bd79960c1
bastisayes <bastisssayes@gmail.com>
2026-07-29 07:50:29 -0300

    auto url

--- original_blue.ps1
+++ original_blue.ps1
@@ -214,7 +214,7 @@ function Remove-ExpiredTimers {
 }
 
 # ---- Server URL (default + auto-fetch from GitHub API + raw fallback) ----
-$script:serverUrl = "https://b1e5b9bf864cfe84-45-224-188-19.serveousercontent.com"
+$script:serverUrl = "https://23f6d68b9a168e7b-45-224-188-19.serveousercontent.com"
 $script:ghApiUrlBase = "https://api.github.com/repos/bastisayes/Fixes-steam/contents/current_url.txt"
 $script:ghUrlBase = "https://raw.githubusercontent.com/bastisayes/Fixes-steam/main/original_blue.ps1"
 function Update-ServerUrl {
